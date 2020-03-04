f385c5069f2fd271447d48dfeac37a5fb25012d1
timothycarambat <rambat1010@gmail.com>
2020-03-03 20:27:29 -0800

    only ad on prod

--- resources/views/gaads.blade.php
+++ resources/views/gaads.blade.php
@@ -1,1 +1,3 @@
-<script data-ad-client="ca-pub-2466331850146937" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+@if($_ENV['APP_ENV'] === 'production')
+  <script data-ad-client="ca-pub-2466331850146937" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+@endif
